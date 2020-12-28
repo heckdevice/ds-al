8112f8358ecd5b98233a9d07d2f1ae8fe269c7a5
s0p04k0 <s0p04ko@homeoffice.wal-mart.com>
2020-12-29 02:31:17 +0530

    - Simple filter functions

--- main.go
+++ main.go
@@ -13,6 +13,38 @@ func linkedList() {
 	ll.AppendNodes(3, 4, 5, 6)
 	fmt.Println(ll.ToString())
 }
+
+func filter(data []int, f func(int) bool) []int {
+	var filteredData []int
+	for _, val := range data {
+		if f(val) == true {
+			filteredData = append(filteredData, val)
+		}
+	}
+	return filteredData
+}
+
+func isEven(n int) bool {
+	if n%2 == 0 {
+		return true
+	}
+	return false
+}
+
+func isOdd(n int) bool {
+	return !isEven(n)
+}
 func main() {
-	linkedList()
+	//linkedList()
+	var dataPoints []int
+	i := 0
+	for i < 100 {
+		i++
+		dataPoints = append(dataPoints, i)
+	}
+	fmt.Println(dataPoints)
+	evenNums := filter(dataPoints, isEven)
+	oddNums := filter(dataPoints, isOdd)
+	fmt.Println(evenNums)
+	fmt.Println(oddNums)
 }
